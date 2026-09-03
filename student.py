db4efde3404be2445d1ee32e9c53b7608fcc6e00
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-09-03 11:26:05 +0530

    students details

---
+++ student.py
@@ -0,0 +1,23 @@
+import sys
+def total(a, b,c):
+    return a+b+c
+def avg(a,b,c):
+    return a+b+c/3
+def result1(a,b,c):
+    if(a>=40 and b>=40 and c>=40):
+        return "pass"
+    else:
+        return "Fail"            
+if __name__ == "__main__":
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+    num3=  int(sys.argv[3])
+    result = total(num1, num2)
+    result1=avg(num1,num2)
+    result2=mul_result1(num1,num2)
+    print("=================================")
+    print("Student details")
+    print("=================================")
+    print(f"sum          : {result}")
+    print(f"avg        : {result1}")
+    print(f"result        : {result2}")
