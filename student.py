9cf363e7d7b279507008bf10d8c674677dac165c
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-09-03 11:37:29 +0530

    students details

--- student.py
+++ student.py
@@ -3,7 +3,7 @@ def total(a, b,c):
     return a+b+c
 def avg(a,b,c):
     return a+b+c/3
-def result1(a,b,c):
+def result(a,b,c):
     if(a>=40 and b>=40 and c>=40):
         return "pass"
     else:
@@ -12,12 +12,12 @@ if __name__ == "__main__":
     num1 = int(sys.argv[1])
     num2 = int(sys.argv[2])
     num3=  int(sys.argv[3])
-    result = total(num1, num2,num3)
-    result1=avg(num1,num2,num3)
-    result2=result1(num1,num2,num3)
+    result1= total(num1, num2,num3)
+    result2=avg(num1,num2,num3)
+    result3=result(num1,num2,num3)
     print("=================================")
     print("Student details")
     print("=================================")
-    print(f"sum          : {result}")
-    print(f"avg        : {result1}")
-    print(f"result        : {result2}")
+    print(f"sum          : {result1}")
+    print(f"avg        : {result2}")
+    print(f"result        : {result3}")
